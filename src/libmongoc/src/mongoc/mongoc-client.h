e323f969cb3b981b7c0078c92834d1680bd3cf3c
Kyle Kloberdanz <kyle.kloberdanz@mongodb.com>
2024-03-20 15:24:42 -0500

    can call oidc_callback stub

--- src/libmongoc/src/mongoc/mongoc-client.h
+++ src/libmongoc/src/mongoc/mongoc-client.h
@@ -32,7 +32,6 @@
 #include "mongoc-macros.h"
 #include "mongoc-read-prefs.h"
 #include "mongoc-server-api.h"
-#include "mongoc-oidc-callback.h"
 #ifdef MONGOC_ENABLE_SSL
 #include "mongoc-ssl.h"
 #endif
@@ -79,6 +78,9 @@ typedef struct _mongoc_client_t mongoc_client_t;
 typedef struct _mongoc_client_session_t mongoc_client_session_t;
 typedef struct _mongoc_session_opt_t mongoc_session_opt_t;
 typedef struct _mongoc_transaction_opt_t mongoc_transaction_opt_t;
+
+typedef struct _mongoc_oidc_callback_params_t mongoc_oidc_callback_params_t;
+typedef struct _mongoc_oidc_credential_t mongoc_oidc_credential_t;
 
 /**
  * mongoc_stream_initiator_t:
@@ -258,7 +260,7 @@ MONGOC_EXPORT (bool)
 mongoc_client_set_server_api (mongoc_client_t *client, const mongoc_server_api_t *api, bson_error_t *error);
 
 MONGOC_EXPORT (void)
-mongoc_client_set_oidc_callback(mongoc_client_t *client,  mongoc_oidc_credential_t (*oidc_callback)(mongoc_oidc_callback_params_t *));
+mongoc_client_set_oidc_callback(mongoc_client_t *client,  mongoc_oidc_credential_t *(*oidc_callback)(mongoc_oidc_callback_params_t *));
 
 MONGOC_EXPORT (mongoc_server_description_t *)
 mongoc_client_get_handshake_description (mongoc_client_t *client, uint32_t server_id, bson_t *opts, bson_error_t *error)
--- src/libmongoc/src/mongoc/mongoc-oidc-callback.h
+++
@@ -1,7 +0,0 @@
-#ifndef MONGOC_OIDIC_CALLBACK_H
-#define MONGOC_OIDIC_CALLBACK_H
-
-typedef struct _mongoc_oidc_callback_params_t mongoc_oidc_callback_params_t;
-typedef struct _mongoc_oidc_credential_t mongoc_oidc_credential_t;
-
-#endif /* MONGOC_OIDIC_CALLBACK_H */
