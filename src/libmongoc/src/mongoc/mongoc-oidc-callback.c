ab486be5213c1040dc1cbec1d4bac5a4b5e98494
Kyle Kloberdanz <kyle.kloberdanz@mongodb.com>
2024-03-20 14:47:28 -0500

    fix function names and type signatures

--- src/libmongoc/src/mongoc/mongoc-oidc-callback.c
+++ src/libmongoc/src/mongoc/mongoc-oidc-callback.c
@@ -25,13 +25,13 @@ mongoc_oidc_callback_params_get_version(mongoc_oidc_callback_params_t* callback_
 }
 
 char *
-mongoc_oidc_redential_get_access_token(mongoc_oidc_credential_t *credential)
+mongoc_oidc_credential_get_access_token(mongoc_oidc_credential_t *credential)
 {
     return credential->access_token;
 }
 
 int64_t
-mongoc_oidc_redential_get_expires_in_seconds(mongoc_oidc_credential_t *credential)
+mongoc_oidc_credential_get_expires_in_seconds(mongoc_oidc_credential_t *credential)
 {
     return credential->expires_in_seconds;
 }
